53839c0ecafb8fda7c3552d405111e5c8c117649
Sibiduser86 <sibiduser@yandex.ru>
2021-12-21 15:22:30 +0000

    3.0

--- run.sh
+++ run.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 source install-docker.sh
+echo "Installing docker..."
 source post-docker.sh
+echo "Manage Docker as a non-root user"
 source start-nginx.sh
+echo "Installing Nginx in Docker"
 source start-flask.sh
+echo "Installing Flask App in Docker"
